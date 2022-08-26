6efb2c24170755c20d94929c1d859ddbf3feb2c6
YoussefLMT <mcyoussef324@gmail.com>
2022-08-26 18:03:11 +0100

    add the login form

--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,10 @@
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
 }
+
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 </style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="home"> 
-    <h1>Login</h1>
-     </div>
+  <div class="login">
+      <form>
+        <input type="email" placeholder="Enter your email">
+        <input type="password" placeholder="Enter your password">
+        <button type="button">Login</button>
+      </form>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeView',
+    name: 'HomeView',
 }
 </script>
