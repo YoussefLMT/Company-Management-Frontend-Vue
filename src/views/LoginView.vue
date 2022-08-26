7e59f2233c791cb5ebe716bca5ce7da1b66e5958
YoussefLMT <mcyoussef324@gmail.com>
2022-08-26 18:15:26 +0100

    add style for the login view

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,15 +1,67 @@
 <template>
-  <div class="login">
-      <form>
+<div class="login">
+    <form>
+        <p>LOGIN</p>
         <input type="email" placeholder="Enter your email">
         <input type="password" placeholder="Enter your password">
         <button type="button">Login</button>
-      </form>
-  </div>
+    </form>
+</div>
 </template>
 
 <script>
 export default {
-    name: 'HomeView',
+    name: 'LoginView',
 }
 </script>
+
+<style scoped>
+.login {
+    display: flex;
+    width: 800px;
+    height: 500px;
+    border: 1px solid black;
+    margin: auto;
+    margin-top: 50px;
+}
+
+form {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    width: 50%;
+    background-color: aqua;
+}
+
+form p {
+    font-size: 30px;
+    font-weight: 700;
+    margin-top: 40px;
+}
+
+form input {
+    padding: 12px;
+    width: 65%;
+    margin: 15px;
+    border: 1px solid black;
+    outline: none;
+    border-radius: 20px;
+}
+
+form button {
+    padding: 12px 30px;
+    width: 40%;
+    margin-top: 40px;
+    background-color: black;
+    font-weight: bold;
+    border: none;
+    outline: none;
+    border-radius: 20px;
+    cursor: pointer;
+    color: white;
+}
+
+form button:hover {
+    background-color: #ccc;
+}
+</style>
