6f7ff13962d2aeba2d2f00d9e1e777e8fdf7dd7f
YoussefLMT <mcyoussef324@gmail.com>
2022-08-27 21:05:03 +0100

    fix some errors

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
 import DashboardView from '../views/DashboardView.vue'
+import store from '../store';
 
 
 const routes = [
@@ -26,7 +27,7 @@ const router = createRouter({
 
 
 router.beforeEach((to, from, next) => {
-  if (to.meta.requiresAuth && !store.state.user.data.token) {
+  if (to.meta.requiresAuth && !store.state.user.token) {
     next({ name: 'login' });
   } else{
     next()
