e70bdae2a50bc153cbc45bd9adc8e90666fc0bcd
YoussefLMT <mcyoussef324@gmail.com>
2022-08-28 12:32:33 +0100

    add table for departments management

--- src/views/DepartmentsView.vue
+++ src/views/DepartmentsView.vue
@@ -3,7 +3,29 @@
     <SideBar />
     <main>
         <h1>Departments</h1>
-        <p>this is a departments page</p>
+        <div class="card dep" style="width: 800px;">
+            <div class="card-header">
+                Departments Managment
+            </div>
+            <div class="card-body">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">id</th>
+                            <th scope="col">department name</th>
+                            <th scope="col">actions</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </main>
 </div>
 </template>
@@ -27,6 +49,10 @@ export default {
 .app main {
     flex: 1 1 0;
     padding: 32px;
+}
+
+.dep {
+    margin: 100px auto;
 }
 
 @media (max-width: 768px) {
