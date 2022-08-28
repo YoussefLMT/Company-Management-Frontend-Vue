dd3165bdaad3480fb5f00e5fe0aa23ba71c3b422
YoussefLMT <mcyoussef324@gmail.com>
2022-08-28 12:43:29 +0100

    add modal for adding new department

--- src/views/DepartmentsView.vue
+++ src/views/DepartmentsView.vue
@@ -6,6 +6,11 @@
         <div class="card dep" style="width: 800px;">
             <div class="card-header">
                 Departments Managment
+
+                <!-- Button trigger modal -->
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Add Department
+                </button>
             </div>
             <div class="card-body">
                 <table class="table">
@@ -25,6 +30,25 @@
                 </table>
             </div>
         </div>
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
 </div>
 </template>
@@ -42,10 +66,10 @@ export default {
             departments: [],
         }
     },
-     mounted() {
+    mounted() {
         this.getDepartments()
     },
-    methods:{
+    methods: {
         async getDepartments() {
             try {
                 const response = await axiosInstance.get("/departments")
@@ -71,6 +95,10 @@ export default {
 
 .dep {
     margin: 100px auto;
+}
+
+.btn {
+    float: right
 }
 
 @media (max-width: 768px) {
