e46dca7b19f52c77a107c5e731d476f8f9d6a15d
YoussefLMT <mcyoussef324@gmail.com>
2022-08-28 12:47:10 +0100

    add form for departments modal

--- src/views/DepartmentsView.vue
+++ src/views/DepartmentsView.vue
@@ -8,7 +8,7 @@
                 Departments Managment
 
                 <!-- Button trigger modal -->
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#departmetsModal">
                     Add Department
                 </button>
             </div>
@@ -32,7 +32,7 @@
         </div>
 
         <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal fade" id="departmetsModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
@@ -40,7 +40,12 @@
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        ...
+                        <form>
+                            <div class="mb-3">
+                                <label for="name" class="form-label">Department Name</label>
+                                <input type="text" class="form-control" id="name" >
+                            </div>
+                        </form>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
