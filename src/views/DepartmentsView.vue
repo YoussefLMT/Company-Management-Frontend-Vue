afb67b418a425e319b903d040f6a9aa4c2746c59
YoussefLMT <mcyoussef324@gmail.com>
2022-08-28 12:56:42 +0100

    display success message

--- src/views/DepartmentsView.vue
+++ src/views/DepartmentsView.vue
@@ -41,6 +41,9 @@
                     </div>
                     <div class="modal-body">
                         <form>
+                            <div class="alert alert-success" v-if="message">
+                                {{ message }}
+                            </div>
                             <div class="mb-3">
                                 <label for="name" class="form-label">Department Name</label>
                                 <input type="text" class="form-control" id="name" v-model="name">
