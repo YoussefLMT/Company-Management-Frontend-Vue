b3e0ea650de30a26ffb158e67818e4b3a5240aa5
YoussefLMT <mcyoussef324@gmail.com>
2022-08-29 13:32:19 +0100

    add data

--- src/views/UpdateCustomerView.vue
+++ src/views/UpdateCustomerView.vue
@@ -11,23 +11,23 @@
                 <form>
                     <div class="mb-3">
                         <label for="fname" class="form-label">First Name</label>
-                        <input type="text" class="form-control" id="fname">
+                        <input type="text" class="form-control" id="fname" v-model="customer.first_name">
                     </div>
                     <div class="mb-3">
                         <label for="lname" class="form-label">Last Name</label>
-                        <input type="text" class="form-control" id="lname">
+                        <input type="text" class="form-control" id="lname" v-model="customer.last_name">
                     </div>
                     <div class="mb-3">
                         <label for="email" class="form-label">Email</label>
-                        <input type="text" class="form-control" id="email">
+                        <input type="text" class="form-control" id="email" v-model="customer.email">
                     </div>
                     <div class="mb-3">
                         <label for="phone" class="form-label">Phone</label>
-                        <input type="text" class="form-control" id="phone">
+                        <input type="text" class="form-control" id="phone" v-model="customer.phone">
                     </div>
                     <div class="mb-3">
                         <label for="address" class="form-label">Address</label>
-                        <input type="text" class="form-control" id="address">
+                        <input type="text" class="form-control" id="address" v-model="customer.address">
                     </div>
                     <button type="button" class="btn btn-primary">Update Customer</button>
                 </form>
@@ -43,6 +43,19 @@ import SideBar from '../components/SideBar'
 export default {
     components: {
         SideBar
+    },
+    data() {
+        return {
+            customer: {
+                first_name: '',
+                last_name: '',
+                email: '',
+                phone: '',
+                address: ''
+            },
+            message: '',
+            errors: ''
+        }
     },
 }
 </script>
@@ -61,7 +74,6 @@ export default {
 .cus {
     margin: 20px auto;
 }
-
 
 @media (max-width: 768px) {
     .app main {
