3ac75386ad9a493243218a3d8a5c9164c873d942
YoussefLMT <mcyoussef324@gmail.com>
2022-08-30 19:40:53 +0100

    add update profile button

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -11,20 +11,21 @@
                 <form>
                     <div class="mb-3">
                         <label for="name" class="form-label">Name</label>
-                        <input type="text" class="form-control" id="name" >
+                        <input type="text" class="form-control" id="name">
                     </div>
                     <div class="mb-3">
                         <label for="email" class="form-label">Email</label>
-                        <input type="email" class="form-control" id="email" >
+                        <input type="email" class="form-control" id="email">
                     </div>
                     <div class="mb-3">
                         <label for="password" class="form-label">New Password</label>
-                        <input type="password" class="form-control" id="password" >
+                        <input type="password" class="form-control" id="password">
                     </div>
-                     <div class="mb-3">
+                    <div class="mb-3">
                         <label for="password_conf" class="form-label">New Password Confirmation</label>
-                        <input type="password" class="form-control" id="password_conf" >
+                        <input type="password" class="form-control" id="password_conf">
                     </div>
+                    <button type="button" class="btn btn-primary">Update Profile</button>
                 </form>
             </div>
         </div>
