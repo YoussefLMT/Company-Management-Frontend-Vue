9aeba656c8884a2d2838cd3a1b628680e594da46
YoussefLMT <mcyoussef324@gmail.com>
2022-08-30 19:33:42 +0100

    add profile view

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="app">
+    <SideBar />
+    <main>
+        <h3>Welcome ahmed</h3>
+        <div class="card emp" style="width: 800px;">
+            <div class="card-header">
+                Profile Managment
+            </div>
+            <div class="card-body">
+tt
+            </div>
+        </div>
+    </main>
+</div>
+</template>
+
+<script>
+import SideBar from '../components/SideBar'
+
+export default {
+    components: {
+        SideBar
+    },
+}
+</script>
+
+<style scoped>
+.app {
+    display: flex;
+    background: var(--light);
+}
+
+.app main {
+    flex: 1 1 0;
+    padding: 32px;
+}
+
+
+@media (max-width: 768px) {
+    .app main {
+        padding: 96px;
+    }
+}
+</style>
