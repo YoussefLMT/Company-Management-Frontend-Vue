5f03ef2c93063c04f39772b475530bc5b12c8949
YoussefLMT <mcyoussef324@gmail.com>
2022-08-30 18:16:28 +0100

    add update employee view

---
+++ src/views/UpdateEmployeeView.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="app">
+    <SideBar />
+    <main>
+        <h3>Update Employee</h3>
+        <div class="card emp" style="width: 800px;">
+            <div class="card-header">
+                Employees Managment
+            </div>
+        </div>
+    </main>
+</div>
+</template>
+
+<script>
+import SideBar from '../components/SideBar'
+
+export default {
+    components: {
+        SideBar
+    },
+}
+</script>
+
+<style scoped>
+.app {
+    display: flex;
+    background: var(--light);
+}
+
+.app main {
+    flex: 1 1 0;
+    padding: 32px;
+}
+
+.emp {
+    margin: 20px auto;
+}
+
+@media (max-width: 768px) {
+    .app main {
+        padding: 96px;
+    }
+}
+</style>
