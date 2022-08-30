554429aaad8cbeca9c8e93a2b25ffb097d154a36
YoussefLMT <mcyoussef324@gmail.com>
2022-08-30 12:09:33 +0100

    add employees view

---
+++ src/views/EmployeesView.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="app">
+    <SideBar />
+    <main>
+        <h1>Employees</h1>
+    </main>
+</div>
+</template>
+
+<script>
+import SideBar from '../components/SideBar'
+
+export default {
+    components: {
+        SideBar
+    },    
+}
+</script>
+
+<style scoped>
+.app {
+    display: flex;
+    background: var(--light);
+}
+
+.app main {
+    flex: 1 1 0;
+    padding: 32px;
+}
+
+@media (max-width: 768px) {
+    .app main {
+        padding: 96px;
+    }
+}
+</style>
