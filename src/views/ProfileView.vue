ef5fd79a88954ef396e8e6085ebf75559d82e411
YoussefLMT <mcyoussef324@gmail.com>
2022-08-30 19:40:03 +0100

    add profile form

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -3,12 +3,29 @@
     <SideBar />
     <main>
         <h3>Welcome ahmed</h3>
-        <div class="card emp" style="width: 800px;">
+        <div class="card profile" style="width: 800px;">
             <div class="card-header">
                 Profile Managment
             </div>
             <div class="card-body">
-tt
+                <form>
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Name</label>
+                        <input type="text" class="form-control" id="name" >
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="email" >
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">New Password</label>
+                        <input type="password" class="form-control" id="password" >
+                    </div>
+                     <div class="mb-3">
+                        <label for="password_conf" class="form-label">New Password Confirmation</label>
+                        <input type="password" class="form-control" id="password_conf" >
+                    </div>
+                </form>
             </div>
         </div>
     </main>
@@ -36,6 +53,9 @@ export default {
     padding: 32px;
 }
 
+.profile {
+    margin: 50px auto;
+}
 
 @media (max-width: 768px) {
     .app main {
